e09d81455b55643b0bc45792bd94a8c83f8ef1db
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-06-28 01:55:03 +0530

    added table

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,11 +15,40 @@ function App() {
       </div>
       <div className="container-right">
         <form>
-          <input id="search" type="text" placeholder="Search your Location"></input>
+          <input
+            id="search"
+            type="text"
+            placeholder="Search your Location"
+          ></input>
           <button id="searchBtn">
             <i className="fa fa-search"></i>
           </button>
         </form>
+        <table>
+          <thead>
+            <tr>
+              <th className="tableHead" colSpan={2}>Weather Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th className="detailName">Feels Like</th>
+              <th className="detailValue">21°</th>
+            </tr>
+            <tr>
+              <th className="detailName">Wind</th>
+              <th className="detailValue">10km/hr</th>
+            </tr>
+            <tr>
+              <th className="detailName">Humidity</th>
+              <th className="detailValue">60%</th>
+            </tr>
+            <tr>
+              <th className="detailName">Cloudy</th>
+              <th className="detailValue">90%</th>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
