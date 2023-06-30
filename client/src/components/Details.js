df7ea14973815d930443012de6b10bab1e0cff17
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-06-30 12:30:55 +0530

    added units

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,16 +2,29 @@ import React from "react";
 import _ from "lodash";
 
 function Details(props) {
-  const { main: details, wind, clouds } = props.weatherData;
-  
+  let { main: details, wind: {speed: wind}, clouds: {all: clouds} } = props.weatherData;
+  details = { ...details, wind, clouds };
+
+  const units = {
+    temp: "°C",
+    feels_like: "°C",
+    temp_min: "°C",
+    temp_max: "°C",
+    pressure: "hPa",
+    humidity: "%",
+    wind: "m/s",
+    clouds: "%"
+  };
+
   const detailArr = Object.keys(details).map((key) => {
     return (
       <tr key={key}>
         <th className="detailName">{_.toUpper(_.replace(key, "_", " "))}</th>
-        <th className="detailValue">{details[key]}</th>
+        <th className="detailValue">{details[key]} <span className="units">{units[key] || ""}</span></th>
       </tr>
     );
   });
+  
   return (
     <table>
       <thead>
